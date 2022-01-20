a3eabfba3329c91fd4f839b9ee1e7ca194d0e3f1
Gabe Joseph <gjoseph92@gmail.com>
2022-01-20 14:02:31 -0700

    Single-source version from poetry

--- geogif/__init__.py
+++ geogif/__init__.py
@@ -1,5 +1,11 @@
-__version__ = "0.1.0"
-
 from .gif import dgif, gif
+
+# Single-source version from pyproject.toml: https://github.com/python-poetry/poetry/issues/273#issuecomment-769269759
+# Note that this will be incorrect for local installs
+import importlib.metadata
+
+__version__ = importlib.metadata.version("stackstac")
+del importlib
+
 
 __all__ = ["gif", "dgif"]
